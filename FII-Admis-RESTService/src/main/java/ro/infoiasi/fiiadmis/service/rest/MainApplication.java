ee95124d6f13e615846901bc7d103a6b1142aa13
mirelap <mirela@popoveniuc.com>
2014-04-26 21:57:45 +0300

    Added logger and updated the API documentation.

---
+++ FII-Admis-RESTService/src/main/java/ro/infoiasi/fiiadmis/service/rest/MainApplication.java
@@ -0,0 +1,33 @@
+package ro.infoiasi.fiiadmis.service.rest;
+
+import org.apache.log4j.Logger;
+import org.restlet.Application;
+import org.restlet.Restlet;
+import org.restlet.resource.ServerResource;
+import org.restlet.routing.Router;
+
+import ro.infoiasi.fiiadmis.service.rest.resources.CandidatesResource;
+
+public class MainApplication extends Application {
+
+    private static final Logger LOG = Logger.getLogger(MainApplication.class);
+
+    /**
+     * Creates a root Restlet that will receive all incoming calls.
+     */
+    @Override
+    public synchronized Restlet createInboundRoot() {
+        LOG.debug("Routing each call to a new respective instance of resource");
+
+        Router router = new Router(getContext());
+
+        attach(router, "/candidates", CandidatesResource.class);
+
+        return router;
+    }
+    
+    private static void attach(Router router, String uriPath, Class<? extends ServerResource> resource) {
+        LOG.debug("Attaching " + uriPath + " to resource " + resource.getSimpleName());
+        router.attach(uriPath, resource);
+    }
+}
--- FII-Admis-RESTService/src/main/java/ro/infoiasi/fiiadmis/service/rest/resources/CandidatesResource.java
+++ FII-Admis-RESTService/src/main/java/ro/infoiasi/fiiadmis/service/rest/resources/CandidatesResource.java
@@ -1,13 +1,30 @@
 package ro.infoiasi.fiiadmis.service.rest.resources;
 
+import java.util.List;
+
+import org.apache.log4j.Logger;
 import org.restlet.ext.json.JsonRepresentation;
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
 
+import ro.infoiasi.fiiadmis.dao.CandidatesDaoImpl;
+import ro.infoiasi.fiiadmis.model.Candidate;
+
 public class CandidatesResource extends ServerResource {
+
+    private static final Logger LOG = Logger.getLogger(CandidatesResource.class);
 
     @Get
     public JsonRepresentation getCandidates() {
-        return new JsonRepresentation("{\"candidates\": {}}");
+        LOG.debug("Retrieving the candidates from the DAO.");
+
+        // Retrieving data from the DAO.
+        List<Candidate> candidates = new CandidatesDaoImpl("db").getAllCandidates(null);
+
+        // TODO mirelap translate List<Candidate> to json
+        String responseText = "{\"candidates\": {}}";
+
+        // Response.
+        return new JsonRepresentation(responseText);
     }
 }
